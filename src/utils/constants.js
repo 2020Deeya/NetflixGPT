2d75855875f357d4b2cf2f1c213bfef5c07cdc83
2020Deeya <dishuupadhyay01@gmail.com>
2024-01-09 00:52:31 +0530

    Built GPT Search Support

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,11 +5,11 @@ export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
 
 export const API_OPTIONS = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNjMxMjI0YzdlYTM0ZDc2ZTRmZTQ2M2VkOWIxMzgwYSIsInN1YiI6IjY1OTY2ZjI1NjBjNTFkNGM0ZTk3ODRiMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.48W8kl3CvxMPWgCCUfxEomeC9DYxYz24yTCwG7KUArc'
-  }
+    accept: "application/json",
+    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
+  },
 };
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
