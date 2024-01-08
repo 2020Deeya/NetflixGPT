233eb096d9917e9a587b377d9d87ccb404512454
2020Deeya <dishuupadhyay01@gmail.com>
2024-01-08 14:28:37 +0530

    TMDB API Integration and built MainContainer

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 import useReducer from './userSlice';
+import moviesSlice from './moviesSlice';
 
 const appStore = configureStore({
     reducer: {
         // here we can combine several slice reducers into one root reducer
-        user : useReducer
+        user : useReducer,
+        movies : moviesSlice,
     }
 });
 
